5b9c8d06bcf4002dff5dac87a9b1a530bc1c4479
vtran91 <vtran16@bellsouth.net>
2023-02-13 17:37:38 +0000

    Ternary to return x if true, y if false

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -20,6 +20,6 @@ public class Lab {
      * @return x if bool is true. if bool is false, return y.
      */
     public int ifExample(boolean bool, int x, int y){
-        return 0;
+        return bool? x : y;
     }
 }
